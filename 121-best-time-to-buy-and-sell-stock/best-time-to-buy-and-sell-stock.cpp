cd9b7183adb5776b9ebb63d740e576e41b250bf4
klbishnoi561 <khilery665@gmail.com>
2026-09-13 18:44:49 +0530

    Time: 166 ms (6.44%) | Memory: 158.2 MB (5.18%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -54,12 +54,12 @@ public:
         int n = arr.size();
         int k = 2;
         vector<vector<int>> dp(n+1,vector<int>(k+1));
-        for(int i=0;i<=n;i++){
-            dp[i][0] = 0;
-        }
-        for(int j=0;j<=k;j++){
-            dp[n][j]=0;
-        }
+        // for(int i=0;i<=n;i++){
+        //     dp[i][0] = 0;
+        // }
+        // for(int j=0;j<=k;j++){
+        //     dp[n][j]=0;
+        // }
         for(int i=n-1;i>=0;i--){
             for(int j=1;j<=k;j++){
                 if(j==2) dp[i][j] = max(dp[i+1][j-1]-arr[i],dp[i+1][j]);
