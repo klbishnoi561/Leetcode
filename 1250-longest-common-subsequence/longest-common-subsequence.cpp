65656da4fa1bc6cdc31d5d1697921c51d88edb44
klbishnoi561 <khilery665@gmail.com>
2026-09-13 13:58:02 +0530

    Time: 27 ms (58.81%) | Memory: 27.4 MB (72.07%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -20,21 +20,43 @@ public:
 
 
     // MEMOIZATION
-    int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp,int n,int m){
-        if(i==n || j==m) return 0;
-        if(dp[i][j]!=-1) return dp[i][j];
-        if(s1[i]==s2[j]) return dp[i][j] = 1 + solve(i+1,j+1,s1,s2,dp,n,m);
-        int c1 = solve(i+1,j,s1,s2,dp,n,m);
-        int c2 = solve(i,j+1,s1,s2,dp,n,m);
-        return dp[i][j]=max(c1,c2);
-    }
+    // int solve(int i,int j,string& s1,string& s2,vector<vector<int>>& dp,int n,int m){
+    //     if(i==n || j==m) return 0;
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     if(s1[i]==s2[j]) return dp[i][j] = 1 + solve(i+1,j+1,s1,s2,dp,n,m);
+    //     int c1 = solve(i+1,j,s1,s2,dp,n,m);
+    //     int c2 = solve(i,j+1,s1,s2,dp,n,m);
+    //     return dp[i][j]=max(c1,c2);
+    // }
+    // int longestCommonSubsequence(string text1, string text2) {
+    //     int n = text1.size();
+    //     int m = text2.size();
+    //     vector<vector<int>> dp(n,vector<int>(m,-1));
+    //     return solve(0,0,text1,text2,dp,n,m); 
+    // }
+
+
+
+    // TABULATION
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size();
         int m = text2.size();
-        vector<vector<int>> dp(n,vector<int>(m,-1));
-        return solve(0,0,text1,text2,dp,n,m); 
+        vector<vector<int>> dp(n+1,vector<int>(m+1));
+        for(int i=0;i<=n;i++){
+            dp[i][m]=0;
+        }
+        for(int j=0;j<=m;j++){
+            dp[n][j]=0;
+        }
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(text1[i]==text2[j]) dp[i][j] = 1+dp[i+1][j+1];
+                else{
+                    dp[i][j] = max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[0][0];
+
     }
-    // int longestCommonSubsequence(string text1, string text2) {
-        
-    // }
 }; 
